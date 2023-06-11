cddc909557a6365f1552159c3adff7e57988ca95
Olumide Odetunde <odetundeolumide95@gmail.com>
2023-06-11 16:52:38 +0100

    Repo Structure Established

---
+++ cracked_screen_identifier/cracked_screen_identifier/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This projects presenta the complete end to end ML cycle of the cracked_screen_identifier app; an image classification project.',
+    author='Olumide',
+    license='MIT',
+)

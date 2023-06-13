3fbe28cb0a3a79ab0b16d573eb97feea5032a176
Olumide Odetunde <odetundeolumide95@gmail.com>
2023-06-13 08:33:34 +0100

    make dataset script updated

--- cracked_screen_identifier/cracked_screen_identifier/src/data/make_dataset.py
+++ cracked_screen_identifier/cracked_screen_identifier/src/data/make_dataset.py
@@ -2,7 +2,10 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import os
+import glob
+import cv2
+import pandas as pd
 
 
 @click.command()
@@ -15,16 +18,35 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+def main(input_filepath, output_filepath):
+    """ Runs data processing scripts to turn raw data from (../interim) into
+        transformed data ready to be analyzed (saved in ../processed).
+    """
+
+    counter = 0
+    images = []
+    labels = []
+    subdirs = glob.glob(input_filepath + '/*screen')
+
+    for subdir in subdirs:
+        label = os.path.basename(subdir).split('_')[0]
+
+        for file_path in glob.glob(subdir + '/*.jpg'):
+            img = cv2.imread(file_path)
+            new_filename = f'image_{counter}.jpg'
+            new_filepath = os.path.join(output_filepath, new_filename)
+            cv2.imwrite(new_filepath, img)
+            images.append(new_filename)
+            labels.append(label)
+            counter += 1
+
+    label = pd.DataFrame({'image': images, 'label': labels})
+    label.to_csv(os.path.join(output_filepath, 'labels.csv'), index=False)
+    logger = logging.getLogger(__name__)
+    logger.info('making interim data set from raw data')
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()

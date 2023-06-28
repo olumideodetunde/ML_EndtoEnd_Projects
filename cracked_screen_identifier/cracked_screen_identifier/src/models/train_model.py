364005f11588a8ebcafb6271bf7eb813099a4de9
Olumide Odetunde <odetundeolumide95@gmail.com>
2023-06-28 21:05:23 +0100

    training script working, to be restructured

--- cracked_screen_identifier/cracked_screen_identifier/src/models/train_model.py
+++ cracked_screen_identifier/cracked_screen_identifier/src/models/train_model.py
@@ -1,28 +1,23 @@
 # -*- coding: utf-8 -*-
-# %%
 import os
 import cv2
 import copy
-import glob
 import torch
-import json
-import random
 import logging
 import numpy as np
 import pandas as pd
-#from tqdm import tqdm
 from torch import nn
 from torch import optim
 import torch.nn.functional as F
-from torchvision import datasets, transforms, models
+from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
 from datetime import datetime
+from model import ModelNet
 from torch.utils.tensorboard import SummaryWriter
 
-
 def main():
-
+##--------- Prepare the data-------------
     epochs = 20
     batch_size = 16
     class PhoneDataset(Dataset):
@@ -62,49 +57,10 @@ def main():
     #Update the transforms
     train_dataset.transform = train_transform
     val_dataset.transform = val_transform
-
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
-    class ModelNet(nn.Module):
-        def __init__(self):
-            super(ModelNet, self).__init__()
-            
-            self.conv1 = nn.Conv2d(in_channels=3, out_channels=8, kernel_size=5, stride=1, padding=1)
-            self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=5, stride=1, padding=1)
-            self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=1)
-            self.conv4 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=1)
-            
-            self.fc1 = nn.Linear(12*12*32, 2048) #Flattened the conv4 output
-            self.fc2 = nn.Linear(2048, 1024)
-            self.fc3 = nn.Linear(1024, 512)
-            self.fc4 = nn.Linear(512, 256)
-            self.fc5 = nn.Linear(256, 2)
-            
-        def forward(self, x):
-            
-            x = F.relu(self.conv1(x))
-            x = F.max_pool2d(x, stride=2, kernel_size=2)
-            
-            x = F.relu(self.conv2(x))
-            x = F.max_pool2d(x, stride=2, kernel_size=2)
-            
-            x = F.relu(self.conv3(x))
-            x = F.max_pool2d(x, stride=2, kernel_size=2)
-            
-            x = F.relu(self.conv4(x))
-            x = F.max_pool2d(x, stride=2, kernel_size=2)
-            
-            x = x.view(-1, 12*12*32)
-            
-            x = F.relu(self.fc1(x))
-            x = F.relu(self.fc2(x))
-            x = F.relu(self.fc3(x))
-            x = F.relu(self.fc4(x))
-            x = self.fc5(x) #No activation
-            
-            return F.log_softmax(x, dim=1)
-
+#--------Trainig loop and exp tracking ---------------
     run_name = f"Run 1 -  Baseline Model"
     log_dir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S") + "-" + run_name
     tb = SummaryWriter(log_dir=log_dir) #Create tensorboard object
@@ -166,4 +122,3 @@ if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
     main()
-# %%

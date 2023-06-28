38c9c6e57b9503ca9dc1c58f8692424c0703dcc6
Olumide Odetunde <odetundeolumide95@gmail.com>
2023-06-28 20:47:15 +0100

    training script working, to be restructured

--- cracked_screen_identifier/cracked_screen_identifier/src/models/train_model.py
+++ cracked_screen_identifier/cracked_screen_identifier/src/models/train_model.py
@@ -1,136 +1,169 @@
 # -*- coding: utf-8 -*-
-#%%
+# %%
 import os
 import cv2
-import json
-import glob
 import copy
+import glob
 import torch
-import click
+import json
+import random
 import logging
+import numpy as np
 import pandas as pd
-from datetime import datetime
-from model import ModelNet
-from torch import nn, optim
-from torchvision import datasets, transforms
+#from tqdm import tqdm
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
 from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
 
-@click.command()
-@click.argument("input_filepath", type=click.Path(exists=True))
-@click.argument("output_filepath", type=click.Path())
 
-## ---- Class Dataset -----
-class ScreenDataset(Dataset):
-    
-    def __init__(self, data_dir, label_csv, transform):
+def main():
+
+    epochs = 20
+    batch_size = 16
+    class PhoneDataset(Dataset):
+        def __init__(self, dir, csv_file, transform): 
+            
+            self.dir = dir
+            self.csv_file = pd.read_csv(csv_file)
+            self.csv_file['label_encoded'] = self.csv_file['label'].apply(lambda x: 0 if x == 'perfect' else 1)
+            self.transform = transform
+            
+        def __len__(self):
+            return len(self.csv_file)
         
-        self.data_dir = data_dir
-        self.label_csv = pd.read_csv(label_csv)
-        # self.label_csv
-        self.label_csv['class_encoded'] = self.label_csv.apply(lambda x: 0 if x['label'] == "perfect" else 1, axis=1)
-        self.transform = transform
-    
-    def __len__(self):
-        return len(self.label_csv)
-    
-    def __getitem__(self, index):
-        
-        filename = os.path.join(self.data_dir, self.label_csv.iloc[index, 1])
-        image = cv2.imread(filename)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = self.transform(image)
-        label = self.label_csv.iloc[index, 3]
-        return image, label
-    
-def main(input_filepath):
-    
-    #train dataset
-    train_dir = os.path.join(input_filepath, "train")
-    print(train_dir)
-    train_label_csv = os.path.join(train_dir, "label.csv")
-    print(train_label_csv)
-    train_transform = transforms.Compose([transforms.ToPILImage(),transforms.ToTensor()])
-    train_dataset = ScreenDataset(train_dir, train_label_csv, train_transform)
-    
-    #validation dataset
-    val_dir = os.path.join(input_filepath, "val")
-    val_label_csv = os.path.join(val_dir, "label.csv")
-    val_transform = repr(transforms.Compose([transforms.ToPILImage(),transforms.ToTensor()]))
-    val_dataset = ScreenDataset(val_dir, val_label_csv, val_transform)
-    
-    #Create dataloader
-    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-    val_loader =  DataLoader(val_dataset, batch_size=16, shuffle=True)
-    
-    #Instantiate the model, criterion, learningrate and optimizer
+        def __getitem__(self, idx):
+            image_name = os.path.join(self.dir, str(self.csv_file.iloc[idx,1]))
+            image = cv2.imread(image_name)
+            image =  torch.from_numpy(image).permute(2,0,1)
+            label = self.csv_file.iloc[idx,3] #label_encoded
+            return image,label
+
+    # Create an instances of the dataset class
+    train_dir = "data/processed/train"
+    train_csv = "data/processed/train/label.csv"
+    train_transform = transforms.Compose([transforms.ToTensor()])
+    train_dataset = PhoneDataset(train_dir, train_csv, transform=train_transform)
+
+    val_dir = "data/processed/val"
+    val_csv = "data/processed/val/label.csv"
+    val_transform = transforms.Compose([transforms.ToTensor()])
+    val_dataset = PhoneDataset(val_dir, val_csv, transform=val_transform)
+
+    train_transform = transforms.Compose([transforms.ToPILImage(),
+                                        transforms.ToTensor(),])
+    val_transform = transforms.Compose([transforms.ToPILImage(),
+                                        transforms.ToTensor(),])
+
+    #Update the transforms
+    train_dataset.transform = train_transform
+    val_dataset.transform = val_transform
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+
+    class ModelNet(nn.Module):
+        def __init__(self):
+            super(ModelNet, self).__init__()
+            
+            self.conv1 = nn.Conv2d(in_channels=3, out_channels=8, kernel_size=5, stride=1, padding=1)
+            self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=5, stride=1, padding=1)
+            self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=1)
+            self.conv4 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=1)
+            
+            self.fc1 = nn.Linear(12*12*32, 2048) #Flattened the conv4 output
+            self.fc2 = nn.Linear(2048, 1024)
+            self.fc3 = nn.Linear(1024, 512)
+            self.fc4 = nn.Linear(512, 256)
+            self.fc5 = nn.Linear(256, 2)
+            
+        def forward(self, x):
+            
+            x = F.relu(self.conv1(x))
+            x = F.max_pool2d(x, stride=2, kernel_size=2)
+            
+            x = F.relu(self.conv2(x))
+            x = F.max_pool2d(x, stride=2, kernel_size=2)
+            
+            x = F.relu(self.conv3(x))
+            x = F.max_pool2d(x, stride=2, kernel_size=2)
+            
+            x = F.relu(self.conv4(x))
+            x = F.max_pool2d(x, stride=2, kernel_size=2)
+            
+            x = x.view(-1, 12*12*32)
+            
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = F.relu(self.fc3(x))
+            x = F.relu(self.fc4(x))
+            x = self.fc5(x) #No activation
+            
+            return F.log_softmax(x, dim=1)
+
+    run_name = f"Run 1 -  Baseline Model"
+    log_dir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S") + "-" + run_name
+    tb = SummaryWriter(log_dir=log_dir) #Create tensorboard object
+
     model = ModelNet()
-    criterion = nn.BCELoss()
-    learning_rate = 0.001
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    
-    #Setup tensorboard to track the training and val metrics
-    experiment_name = F'Exp 1 - Train Model'
-    log_dir = "logs" + datetime.now().strftime("%Y%m%d-%H%M%S") + "-" + experiment_name
-    tb = SummaryWriter(log_dir=log_dir)
-    
-    #Training loop
-    epochs = 15
-    best_loss = float("inf")
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    best_loss = float('inf')
     train_losses = []
-    val_losses = []
-    
-    model.train()
+    valid_losses = []
+
     for epoch in range(1, epochs+1):
-        train_loss = 0.0
-        val_loss = 0.0
-    
-        for data, label in train_loader:
-            optimizer.zero_grad()
-            output = model(data)
-            loss = criterion(output, label)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item() * data.size(0)
         
+        train_loss = 0.0
+        valid_loss = 0.0
+        
+        model.train()
+        for data, label in train_loader:
+            data = data.float()
+            #zero the gradients
+            optimizer.zero_grad()
+            #forward pass
+            output = model(data.float())
+            #Calculate batch loss
+            loss = criterion(output, label)
+            #backward pass
+            loss.backward()
+            #Optimize the weights
+            optimizer.step()
+            #update training loss
+            train_loss += loss.item() * data.size(0)
+            
+        #validate the model
         model.eval()
         for data, label in val_loader:
+            data = data.float()
             output = model(data)
             loss = criterion(output, label)
-            val_loss += loss.item() * data.size(0)
-        
+            valid_loss += loss.item() * data.size(0)
+            
         #Calculate the average losses
         train_loss = train_loss/len(train_loader.sampler)
         valid_loss = valid_loss/len(val_loader.sampler)
         train_losses.append(train_loss)
-        val_losses.append(val_loss)
+        valid_losses.append(valid_loss)
         
-        #Log the loss on tensorboard and print updates
-        tb.add_scalars("Loss", {"Train": train_loss, "Val":val_loss}, epoch)
-        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:6f}'.format(epoch, train_loss, val_loss))
+        tb.add_scalars("Loss", {"Train": train_loss, "Val": valid_loss}, epoch)
+        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(epoch, train_loss, valid_loss))
         
-        #Save the best model
-        if val_loss < best_loss:
-            best_loss = val_loss
+        #Save the model if validation loss has decreased
+        if valid_loss < best_loss:
+            best_loss = valid_loss
             best_model_weights = copy.deepcopy(model.state_dict())
             torch.save(model.state_dict(), f'{log_dir}/modelnet.pth')
-            #print("Saved the new best model")
+            print("Saved the new best model")
     tb.close()
-    
-    #Save parameters and hyperparameters per training 
-    parameters = {
-    "epochs": epochs,
-    "learning_rate": learning_rate,
-    "optimizer": optimizer.__class__.__name__,
-    "criterion": criterion.__class__.__name__,
-    "model": model.__class__.__name__,}
-    with open(f'{log_dir}/parameters.json', 'w') as fp:
-        json.dump(parameters, fp)
-        
-    logger = logging.getLogger(__name__)
-    logger.info('making interim data set from raw data')
     
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-    main("data/processed")
+    main()
+# %%

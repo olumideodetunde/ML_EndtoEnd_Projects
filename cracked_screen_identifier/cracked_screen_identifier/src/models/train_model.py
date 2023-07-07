24a9847551adfee64445883e19b94a79fd763daf
Olumide Odetunde <odetundeolumide95@gmail.com>
2023-07-07 18:07:48 +0100

    train_model debugged

--- cracked_screen_identifier/cracked_screen_identifier/src/models/train_model.py
+++ cracked_screen_identifier/cracked_screen_identifier/src/models/train_model.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#%%
+
 import os
 import cv2
 import json
@@ -9,7 +9,7 @@ import torch
 import logging
 from datetime import datetime
 from model_architecture import ModelNet
-from dataloader import main as dataloader_main
+from dataloader import PhoneDataset
 from torch.utils.tensorboard import SummaryWriter
 
 class Trainer:
@@ -19,13 +19,25 @@ class Trainer:
         self.epochs = epochs
         self.learning_rate = learning_rate
         self.model = ModelNet()
-        self.criterion =  torch.nn.LLLoss()
+        self.criterion =  torch.nn.NLLLoss()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
         self.best_loss = float("inf")
         self.train_losses = []
         self.val_losses = []
         self.tb =  None
         
+    def prepare_dataset(self,):
+        train_data_dir = "data/processed/train"
+        train_csv = "data/processed/train/label.csv"
+        train_dataset = PhoneDataset(train_data_dir, train_csv)
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
+    
+        val_data_dir = "data/processed/val"
+        val_csv = "data/processed/val/label.csv"
+        val_dataset = PhoneDataset(val_data_dir, val_csv)    
+        val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=2, shuffle=True)
+        return train_loader, val_loader
+    
     def train_step(self, image, label):
         image = image.float()
         self.optimizer.zero_grad()
@@ -39,6 +51,8 @@ class Trainer:
         experiment_name = f"Experiment 1 - Baseline Model"
         log_dir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S") + "-" + experiment_name
         self.tb = SummaryWriter(log_dir=log_dir)
+        train_loader, val_loader = self.prepare_dataset()
+
         
         for epoch in range(1, self.epochs+1):
             
@@ -47,11 +61,11 @@ class Trainer:
             
             self.model.train()
             for image, label in train_loader:
-                train_loss = self.train_step(image, label)
+                train_loss += self.train_step(image, label)
             
             self.model.eval()
             for image, label in val_loader:
-                val_loss =  self.train_step(image, label)
+                val_loss +=  self.train_step(image, label)
                 
             train_loss =  train_loss / len(train_loader.sampler)
             val_loss =  val_loss / len(val_loader.sampler)
@@ -81,13 +95,12 @@ class Trainer:
     def run_training(self):
         self.train_model()
 
-@click.command()
-@click.option("--epochs", default=10, help="Number of epochs")
-@click.option("--learning-rate", default=0.001, help="Learning rate")
-def main(epochs, batch_size, learning_rate):
-    train_loader, val_loader = main("data/processed")
-    trainer = Trainer(epochs, batch_size, learning_rate)
+# @click.command()
+# @click.option("--epochs", default=10, help="Number of epochs")
+# @click.option("--learning-rate", default=0.001, help="Learning rate")
+def main(epochs,  learning_rate):
+    trainer = Trainer(epochs, learning_rate)
     trainer.run_training()
 
 if __name__ == "__main__":
-    main()
+    main(epochs=10, learning_rate=0.001)

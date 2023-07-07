a3fcffc50b17c8ee3a770a37cc75e1aec44e7617
Olumide Odetunde <odetundeolumide95@gmail.com>
2023-07-07 18:07:03 +0100

    Datalaoder updated and synced with train_model

--- cracked_screen_identifier/cracked_screen_identifier/src/models/dataloader.py
+++ cracked_screen_identifier/cracked_screen_identifier/src/models/dataloader.py
@@ -2,11 +2,7 @@
 import os
 import cv2
 import torch
-import click
-import logging
 import pandas as pd
-from pathlib import Path
-from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
@@ -28,22 +24,3 @@ class PhoneDataset(Dataset):
         image =  image.permute(2,1,0)
         label = self.label_csvfile.iloc[idx,3] #label_encoded
         return image,label
-    
-if __name__ == "__main__":
-    
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-    logger = logging.getLogger(__name__)
-    logger.info('making dataset and dataloader from val and train dir')
-    
-    train_data_dir = "data/processed/train"
-    train_csv = "data/processed/train/label.csv"
-    train_dataset = PhoneDataset(train_data_dir, train_csv)
-    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-    
-    val_data_dir = "data/processed/val"
-    val_csv = "data/processed/val/label.csv"
-    val_dataset = PhoneDataset(val_data_dir, val_csv)    
-    val_loader = DataLoader(val_dataset, batch_size=2, shuffle=True)
-    
-    
